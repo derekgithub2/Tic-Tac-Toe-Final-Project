d5cbb75e6e3cb24f5f8fdcd6e244420a7508c5bb
Derek Yeh <derek.yeh91@gmail.com>
2022-11-09 14:24:41 -0800

    feat: Update game and player classes

--- game.js
+++ game.js
@@ -1,14 +1,60 @@
+const Player = require("./player");
+
 class Game {
-    constructor() {
-        this.board = 
-        
+  constructor(player1, player2) {
+    this.board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    this.player1 = new Player(1, "x");
+    this.player2 = new Player(2, "o");
+    this.currentPlayer = this.player1;
+    this.turnCounter = 0;
+    // so each instance of game has 2 new instances of players in the constructor
+  }
+
+  // function to determine who's turn it is
+  changeTurn() {
+    this.turnCounter++;
+    if (this.turnCounter % 2 === 1) {
+      this.player1.turn = true;
+      this.player2.turn = false;
+      this.currentPlayer = this.player1;
+    } else if (this.turnCounter % 2 === 0) {
+      this.player2.turn = true;
+      this.player1.turn = false;
+      this.currentPlayer = this.player2;
     }
+  }
+
+  // function to change array when player makes move
+  makeMove(indexOfMove) {
+    if (this.currentPlayer.turn === true) {
+      this.board.splice(indexOfMove, 1, this.currentPlayer.token);
+    }
+  }
+
+  // function to determine winner switch-statement
+  checkBoard() {
+    switch (this.currentPlayer.token) {
+      case this.board[0] && this.board[1] && this.board[2]:
+      case this.board[3] && this.board[4] && this.board[5]:
+      case this.board[6] && this.board[7] && this.board[8]:
+      case this.board[0] && this.board[4] && this.board[8]:
+      case this.board[0] && this.board[3] && this.board[6]:
+      case this.board[1] && this.board[4] && this.board[7]:
+      case this.board[2] && this.board[5] && this.board[8]:
+      case this.board[2] && this.board[4] && this.board[6]:
+        this.currentPlayer.increaseWins();
+        console.log(`${this.currentPlayer.token} wins!`);
+        break;
+    }
+  }
 }
 
+module.exports = Game;
+
 // include 2 Player instances
-// a way to keep track of data for the game board
-// a way to keep track of whos turn it is 
-// check the game's board data for win conditions
+// a way to keep track of data for the game board --> array
+// a way to keep track of whos turn it is --> changeTurn()
+// check the game's board data for win conditions --> checkBoard() or determineWinner()
 // detect when a game is a draw (no one has won)
-// way to reset the Game's board to begin a new game 
-    // a short timeout is used after a comppleted game to reset the board. 
+// way to reset the Game's board to begin a new game
+// a short timeout is used after a comppleted game to reset the board.
--- main.js
+++ main.js
@@ -1,20 +1,83 @@
 // main.js contains all DOM related JavaScript
 
 // Global Variables
-// Player 1 Wins Array
-    // 
-// Player 2 Wins Array
-    // 
+const Player = require("./player")
+const Game = require("./game")
+
+// var player1 = new Player()
+// var player2 = new Player()
+var game = new Game
+// player 1 icon
+// player 2 icon
+
 
 // Query Selectors
 
-
+//queryselect the entire board container
+    // event.target in the event listener to specify which box is being clicked.
 
 
 // Event Listeners
+// window.addEventListener('on load', function() {
 
+// })
+    //eventlisteners for whole board container and listen for clicks inside it. 
+        // event.target for the position? or write out event listener for each position?
 
 
 // Functions and Event Handlers
 
+// function to place piece when player clicks position
+    // gameBoard.splice(inputPosition, 0, 'player1')
 
+function startGame () {
+    game.changeTurn()
+    console.log(game)
+}
+startGame()
+
+// console.log(game.player1)
+// console.log(game.player2)
+
+// game.makeMove(0)
+// console.log("counter", game.turnCounter)
+// console.log(game.board)
+// console.log("----------------")
+
+// game.changeTurn()
+// console.log("counter", game.turnCounter)
+// game.makeMove(4)
+// console.log(game.board)
+
+// console.log("----------------")
+// game.changeTurn()
+// console.log("counter", game.turnCounter)
+// game.makeMove(1)
+// console.log(game.board)
+
+// console.log("----------------")
+// game.changeTurn()
+// console.log("counter", game.turnCounter)
+// game.makeMove(9)
+// console.log(game.board)
+
+// console.log("----------------")
+// game.changeTurn()
+// console.log("counter", game.turnCounter)
+// game.makeMove(2)
+// console.log(game.board)
+
+// console.log(game.player1)
+// console.log(game.player2)
+game.makeMove(0)
+game.makeMove(1)
+game.makeMove(2)
+game.makeMove(5)
+console.log(game.board)
+game.checkBoard()
+console.log(game.currentPlayer.wins)
+
+
+// function to determine the winner
+    // for loop through array to see what is spliced into the array at what index
+    
